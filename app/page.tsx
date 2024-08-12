31cf8375295adacc58a68349a9a76125799e329c
lakmalmabm <lakmalmabm@gmail.com>
2024-08-13 02:03:50 +0530

    page width

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import ChatSection from "./components/chat-section";
 
 export default function Home() {
   return (
-    <main className="h-screen max-w-2xl flex flex-col justify-center items-center px-2 py-10 overflow-hidden bg-gradient-to-r from-rose-600 from-15% via-red-600 via-55% to-red-700 no-scrollbar border rounded-md">
+    <main className="h-screen max-w-3xl w-full flex flex-col justify-center items-center px-2 py-10 overflow-hidden bg-gradient-to-r from-rose-600 from-15% via-red-600 via-55% to-red-700 no-scrollbar border rounded-md">
       <Header/>
         <div className="h-full w-full flex">
           <ChatSection />
